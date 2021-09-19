63894bb6345a7165d5fa31e088ab3f3df5e6dfb2
nwe <nathan.ehrlich2621@gmail.com>
2021-09-18 21:44:38 -0600

    finished schema and seed files, updated table associations

---
+++ db/schema.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+-- Department Name
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL,
+
+  PRIMARY KEY (id),
+  UNIQUE KEY (department_name)
+);
+
+-- name, salary, and department
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+);
+
+
+-- first name, last name, role, and manager
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) 
+    REFERENCES role(id)
+);
