7789513c1972869351ef6ec4fc07c649ee970ef2
nwe <nathan.ehrlich2621@gmail.com>
2021-09-18 21:43:36 -0600

    started building a class to house all queries

---
+++ db/query_lib.js
@@ -0,0 +1,36 @@
+const db = require('./connection');
+
+class DBQuery {
+  constructor(db){
+    this.db = db
+  }
+
+  getAllDept() {
+
+  }
+
+  getAllRoles() {
+
+  }
+
+  getAllEmp() {
+
+  }
+
+  addDept() {
+
+  }
+
+  addRole() {
+
+  }
+
+  addEmp() {
+
+  }
+
+  updateEmp() {
+    
+  }
+
+}
