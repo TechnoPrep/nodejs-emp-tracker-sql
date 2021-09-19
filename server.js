35fb4606304fae80351d31318523dd26e9f8b75b
nwe <nathan.ehrlich2621@gmail.com>
2021-09-19 13:12:53 -0600

    Built out functions to handle addDept, addRole, addEmp

--- server.js
+++ server.js
@@ -1,62 +1,232 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 const sql = require('./db/query_lib');
-
-// Query lists
+const cHelper = require('./lib/choiceHelper');
 
 // View All Departments
-// Return Name and ID
 const viewDepts = () => {
-  sql
-  .getDepts()
+  sql.getDepts()
+
   .then(([rows]) => {
     console.log('\n');
     console.log(cTable.getTable(rows));
   })
+
   .then(()=> {
       chooseRequest();
   }) 
 }
 
 // View All Roles
-// Return title, role id, department it belongs to, and salary
-
 const viewRoles = () => {
-  sql
-  .getDepts()
+  sql.getRoles()
+
   .then(([rows]) => {
     console.log('\n');
     console.log(cTable.getTable(rows));
   })
+
   .then(()=> {
       chooseRequest();
   }) 
 }
 // View All employees
-// ID, FName, LName, Job Title, Departments, salaries, and Manager
-
 const viewEmps = () => {
-  sql
-  .getEmps()
+  sql.getEmps()
+
   .then(([rows]) => {
     console.log('\n');
     console.log(cTable.getTable(rows));
   })
+
   .then(()=> {
       chooseRequest();
   }) 
 }
 
 // add an department
-// Enter fields: Department Name
+const newDept = async () => {  
+
+  const deptartment = await inquirer.prompt([
+     {
+       type: "input",
+       name: "name",
+       message: "What is the name of the Department",
+       validate: (name) =>{
+         if (name) {
+           return true;
+         } else {
+           console.log(" Please Enter a Department Name!")
+           return false;
+         }
+       },
+    },
+  ]);
+
+  await sql.addDept(deptartment);
+
+  chooseRequest();
+}
 
 // Add a role
-// Enter fields: Name, Salary, Department for role
+const newRole = async () => {
+
+  const choicesArr = await cHelper.deptChoices();
+
+  const role = await inquirer.prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the name of the Role?",
+        validate: (title) =>{
+          if (title) {
+            return true;
+          } else {
+            console.log(" Please Enter a Role Name!")
+            return false;
+          }
+        },
+     },
+     {
+       type: "input",
+       name: 'salary',
+       message: "What is the Salary of the Role?",
+       validate: (salary) =>{
+         if(salary && !isNaN(salary)){
+           return true;
+         } else {
+           console.log(" Please Enter a Role Salary");
+         }
+       }
+     },
+     {
+      type: "list",
+      name: 'department_id',
+      message: "What Department is the Role associated with?",
+      choices: choicesArr,
+      validate: (choices) =>{
+        if(choices){
+          return true;
+        } else {
+          console.log(" Please Enter a Role Salary");
+        }
+      }
+    }
+   ]);
+
+  await sql.addRole(role);
+
+  chooseRequest();  
+ 
+}
 
 // add an employee
-// Enter fields: fname, lname, role, manager
+const newEmp = async () => {
+
+  const roleArr = await cHelper.roleChoices();
+
+  const mgmtArr = await cHelper.mgmtChoices();
+
+  const emp = await inquirer.prompt([
+      {
+        type: "input",
+        name: "first",
+        message: "What is the Employees First Name?",
+        validate: (first) =>{
+          if (first && isNaN(first)) {
+            return true;
+          } else {
+            console.log(" Please Enter a Name!")
+            return false;
+          }
+        },
+     },
+     {
+      type: "input",
+      name: "last",
+      message: "What is the Employees Last Name?",
+      validate: (last) =>{
+        if (last && isNaN(last)) {
+          return true;
+        } else {
+          console.log(" Please Enter a Name!")
+          return false;
+        }
+      },
+    },
+    {
+      type: "list",
+      name: 'role_id',
+      message: "What is the Employees Role?",
+      choices: roleArr
+    },
+    {
+      type: "list",
+      name: 'manager_id',
+      message: "Who is the Employees Manager?",
+      choices: mgmtArr
+    }
+   ]);
+
+  await sql.addEmp(emp);
+
+  chooseRequest();  
+ 
+}
 
 // update an employees role
+const newEmp = async () => {
+
+  const roleArr = await cHelper.roleChoices();
+
+  const mgmtArr = await cHelper.mgmtChoices();
+
+  const emp = await inquirer.prompt([
+      {
+        type: "input",
+        name: "first",
+        message: "What is the Employees First Name?",
+        validate: (first) =>{
+          if (first && isNaN(first)) {
+            return true;
+          } else {
+            console.log(" Please Enter a Name!")
+            return false;
+          }
+        },
+     },
+     {
+      type: "input",
+      name: "last",
+      message: "What is the Employees Last Name?",
+      validate: (last) =>{
+        if (last && isNaN(last)) {
+          return true;
+        } else {
+          console.log(" Please Enter a Name!")
+          return false;
+        }
+      },
+    },
+    {
+      type: "list",
+      name: 'role_id',
+      message: "What is the Employees Role?",
+      choices: roleArr
+    },
+    {
+      type: "list",
+      name: 'manager_id',
+      message: "Who is the Employees Manager?",
+      choices: mgmtArr
+    }
+   ]);
+
+  await sql.updateEmp(emp);
+
+  chooseRequest();  
+ 
+}
 
 
 const chooseRequest = () => {
@@ -65,7 +235,7 @@ const chooseRequest = () => {
         type: 'list',
         name: 'request',
         message: 'What would you like to do?',
-        choices: ['Add A Department',
+        choices: ['Add a Department',
                   'Add an Employee',
                   'Add a Role',
                   'Update Employee',
@@ -76,16 +246,30 @@ const chooseRequest = () => {
         loop: false,
       },
   ])
+
   .then((data) => {
       const {request} = data;
+      console.log(request);
     //   Switch case
     switch (request) {
         case 'View All Departments':
-            viewDept();
+            viewDepts();
             break;
         case 'View All Employees':
             viewEmps();
-            break;          
+            break;
+        case 'View All Roles':
+            viewRoles();
+            break;
+        case 'Add a Department':
+          newDept();
+          break;
+        case 'Add a Role':
+          newRole();
+          break;
+        case 'Add an Employee':
+          newEmp();
+          break;                          
     
         default:
             break;
