1eae0e5d666672031c4e4b934fb8631d1118aafe
nwe <nathan.ehrlich2621@gmail.com>
2021-09-19 13:13:43 -0600

    Built associated queries to handle requests in server.js

--- db/query_lib.js
+++ db/query_lib.js
@@ -6,21 +6,41 @@ class DBQuery {
   }
 
   getDepts() {
-    return this.db.promise().query(
+    return this.db
+      .promise()
+      .query(
     `SELECT * 
     FROM department`
     );
   }
 
   getRoles() {
-    return this.db.promise().query(
-      `SELECT * 
+    return this.db
+      .promise()
+      .query(
+      `SELECT r.title AS Title, 
+      r.salary AS Salary, 
+      d.department_name AS Department
+      FROM role r
+      LEFT JOIN department d
+      ON r.department_id = d.id
+      ORDER BY Department, r.id ASC`
+      );
+  }
+
+  getRoleIds(){
+    return this.db
+      .promise()
+      .query(
+      `SELECT *
       FROM role`
       );
   }
 
   getEmps() {
-    return this.db.promise().query(
+    return this.db
+      .promise()
+      .query(
       `SELECT e.id as 'Employee ID', 
       e.first_name AS 'First Name',
       e.last_name AS 'Last Name',
@@ -39,16 +59,48 @@ class DBQuery {
       );
   }
 
-  addDept() {
-
+  getManagers() {
+    return this.db
+      .promise()
+      .query(
+      `SELECT id, CONCAT(first_name, ' ', last_name) AS manager_name
+      FROM employee 
+      WHERE manager_id IS NULL`
+    )
   }
 
-  addRole() {
-
+  addDept(data) {
+    const values = [data.name];
+    return this.db
+      .promise()
+      .query(
+        `INSERT INTO department (department_name) VALUES(?)`,
+        values
+        );
   }
 
-  addEmp() {
+  addRole(data) {
+    const values = [data.title, data.salary, data.department_id];
+    return this.db
+      .promise()
+      .query(
+        `INSERT INTO role 
+        (title, salary, department_id) 
+        VALUES(?,?,?)`,
+        values
+        );
+  }
 
+  addEmp(data) {
+    const values = [data.first, data.last, data.role_id, data.manager_id];
+    return this.db
+      .promise()
+      .query(
+        `INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+        VALUES(?,?,?,?)`,
+        values
+      )
   }
 
   updateEmp() {
