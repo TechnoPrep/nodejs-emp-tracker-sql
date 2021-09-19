e0dd0540cb54085ddd8c516267cad5f86621bd5f
nwe <nathan.ehrlich2621@gmail.com>
2021-09-19 08:23:57 -0600

    removed ascii-art module

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql2');
-const art = require('ascii-art');
 
 // Creating our connection string function
 const db = mysql.createConnection({
@@ -7,13 +6,6 @@ const db = mysql.createConnection({
   user: 'root', //'process.env.DB_USER',
   password: 'BigMiike60!', //process.env.DB_PASS,
   database: 'company_db' //process.env.DB_NAME
-},
-art.font("Some Text", 'doom')
-       .then((rendered)=>{
-           //rendered is the ascii
-       }).catch((err)=>{
-           //err is an error
-       })
-);
+});
 
 module.exports = db;
